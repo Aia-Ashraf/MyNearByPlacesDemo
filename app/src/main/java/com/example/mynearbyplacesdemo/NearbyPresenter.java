980e41ce6eba6571f68a2c4e1ca6cac42b1aaa26
DELL <aia.ashraf94@gmail.com>
2019-10-25 09:45:33 +0200

    MVP Design Pattern

---
+++ app/src/main/java/com/example/mynearbyplacesdemo/NearbyPresenter.java
@@ -0,0 +1,85 @@
+package com.example.mynearbyplacesdemo;
+
+import android.content.Context;
+import android.location.Location;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mynearbyplacesdemo.models.FoursquareGroup;
+import com.example.mynearbyplacesdemo.models.FoursquareJSON;
+import com.example.mynearbyplacesdemo.models.FoursquareResponse;
+import com.example.mynearbyplacesdemo.models.FoursquareResults;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+class NearbyPresenter {
+    NearbyPresenter(Context context, GoogleApiClient googleApiClient, RecyclerView recyclerView, NearbyView nearbyView) {
+        this.context = context;
+        this.mGoogleApiClient = googleApiClient;
+        this.placePicker = recyclerView;
+        this.nearbyView = nearbyView;
+    }
+
+    private Context context;
+    private NearbyView nearbyView;
+    private GoogleApiClient mGoogleApiClient;
+    private RecyclerView placePicker;
+    private PlacePickerAdapter placePickerAdapter;
+    private String foursquareBaseURL = "https://api.foursquare.com/v2/";
+    private String foursquareClientID = "KKE3GF3FQ3L133GTENHAQF2XWLFDVHXNU0F1S0KCOUBV10BD";
+    private String foursquareClientSecret = "0AEJ403ARKSAAIAOJLBWKJMYXHN2CP22DCQMN3PSKPONHJNU";
+
+
+    void network() {
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if (mLastLocation != null) {
+            String userLL = mLastLocation.getLatitude() + "," + mLastLocation.getLongitude();
+            double userLLAcc = mLastLocation.getAccuracy();
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(foursquareBaseURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            FoursquareService foursquare = retrofit.create(FoursquareService.class);
+            Call<FoursquareJSON> coffeeCall = foursquare.searchCoffee(
+                    foursquareClientID,
+                    foursquareClientSecret,
+                    userLL,
+                    userLLAcc);
+            coffeeCall.enqueue(new Callback<FoursquareJSON>() {
+                @Override
+                public void onResponse(Call<FoursquareJSON> call, Response<FoursquareJSON> response) {
+
+
+                    nearbyView.hideProgressBar();
+                    if (response.body() == null) {
+                        nearbyView.handelingNoDataFound();
+                    } else {
+                        FoursquareJSON fjson = response.body();
+                        FoursquareResponse fr = fjson.response;
+                        FoursquareGroup fg = fr.group;
+                        List<FoursquareResults> frs = fg.results;
+                        placePickerAdapter = new PlacePickerAdapter(context, frs);
+                        placePicker.setAdapter(placePickerAdapter);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<FoursquareJSON> call, Throwable t) {
+                    nearbyView.handelingError();
+                }
+            });
+        } else {
+            nearbyView.handelingError();
+        }
+    }
+
+}
--- app/src/main/java/com/example/mynearbyplacesdemo/PlacePickerActivity.java
+++ app/src/main/java/com/example/mynearbyplacesdemo/PlacePickerActivity.java
@@ -2,53 +2,34 @@
 package com.example.mynearbyplacesdemo;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
-import androidx.core.app.NavUtils;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.mynearbyplacesdemo.models.FoursquareGroup;
-import com.example.mynearbyplacesdemo.models.FoursquareJSON;
-import com.example.mynearbyplacesdemo.models.FoursquareResponse;
-import com.example.mynearbyplacesdemo.models.FoursquareResults;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
-import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PlacePickerActivity extends Activity
         implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, NearbyView {
 
     private GoogleApiClient mGoogleApiClient;
+    private NearbyPresenter nearbyPresenter;
     private ProgressBar progressBar;
     private static final int PERMISSION_ACCESS_FINE_LOCATION = 1;
     private RecyclerView placePicker;
     private LinearLayoutManager placePickerManager;
-    private PlacePickerAdapter placePickerAdapter;
-    private String foursquareBaseURL = "https://api.foursquare.com/v2/";
-    private String foursquareClientID;
-    private String foursquareClientSecret;
-    private Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +39,6 @@ public class PlacePickerActivity extends Activity
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_ACCESS_FINE_LOCATION);
         }
-
 
         showProgressBar();
         placePicker = (RecyclerView) findViewById(R.id.coffeeList);
@@ -73,64 +53,15 @@ public class PlacePickerActivity extends Activity
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
-        foursquareClientID = "KKE3GF3FQ3L133GTENHAQF2XWLFDVHXNU0F1S0KCOUBV10BD";
-        foursquareClientSecret = "0AEJ403ARKSAAIAOJLBWKJMYXHN2CP22DCQMN3PSKPONHJNU";
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+        nearbyPresenter = new NearbyPresenter(getApplicationContext(), mGoogleApiClient, placePicker, this);
+
     }
 
     @Override
     public void onConnected(Bundle connectionHint) {
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-
-            if (mLastLocation != null) {
-                String userLL = mLastLocation.getLatitude() + "," + mLastLocation.getLongitude();
-                double userLLAcc = mLastLocation.getAccuracy();
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl(foursquareBaseURL)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
-                FoursquareService foursquare = retrofit.create(FoursquareService.class);
-                Call<FoursquareJSON> coffeeCall = foursquare.searchCoffee(
-                        foursquareClientID,
-                        foursquareClientSecret,
-                        userLL,
-                        userLLAcc);
-                coffeeCall.enqueue(new Callback<FoursquareJSON>() {
-                    @Override
-                    public void onResponse(Call<FoursquareJSON> call, Response<FoursquareJSON> response) {
-
-
-                        hideProgressBar();
-                        if (response.body() == null) {
-                            handelingNoDataFound();
-                        } else {
-                            FoursquareJSON fjson = response.body();
-                            FoursquareResponse fr = fjson.response;
-                            FoursquareGroup fg = fr.group;
-                            List<FoursquareResults> frs = fg.results;
-                            placePickerAdapter = new PlacePickerAdapter(getApplicationContext(), frs);
-                            placePicker.setAdapter(placePickerAdapter);
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<FoursquareJSON> call, Throwable t) {
-                        handelingError();
-                    }
-                });
-            } else {
-                handelingError();
-            }
+            nearbyPresenter.network();
         }
     }
 
